755bc76a1c1df63f01eafef19984479acb5989fb
Tiago R Lammers <trlammers@gmail.com>
2019-11-18 17:56:20 -0300

    Ordered imports

--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -1,43 +1,32 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
-import { Repository } from '../../store/ducks/repositories/types';
-import * as RepositoriesActions from '../../store/ducks/repositories/actions';
 
+import { Repository } from '../../store/ducks/repositories/types';
 import { ApplicationState } from '../../store';
 
-// tipo das infos que vem do mapstatetoprops
+import * as RepositoriesActions from '../../store/ducks/repositories/actions';
+
 interface StateProps {
   repositories: Repository[]
 }
 
-// Mapeia funcoes que vem to mapdispatch to props
 interface DispatchProps {
   loadRequest(): void
 }
 
-// qualquer outra prop que vem de um component pai
-// interface OwnProps {
-
-// }
-
 type Props = StateProps & DispatchProps
-
-type Caceta = {
-  name: string
-}
 
 class RepositoryList extends Component<Props> {
   componentDidMount() {
     const { loadRequest } = this.props;
+
     loadRequest();
   }
 
   render() {
     const { repositories } = this.props;
-    // const { name } = this.caceta;
-    // console.log(`manameis ${name}`);
+
     return (
       <ul>
         {repositories.map((repository) => repository.name)}
